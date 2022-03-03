d7615dc1f0c71cdc9d296a41fc887e4a36663c06
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2022-03-03 04:09:03 -0300

    Configurando add repo

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-document.querySelector('body').style.background = 'blue';
+class App{
+    // construtor
+    construtor(){
+        // lista de repositórios
+        this.repositories = [];
+
+        // form
+        this.form = document.querySelector('form');
+
+        // método para registrar eventos do formulario
+        this.registerEvent();
+    }
+
+    registerEvent(){
+        this.formulario.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event){
+        // Evita que o formulário recarregue a página
+        event.preventDefault();
+
+        // Adiciona o repositório na lista
+        this.repositories.push({
+            name: '',
+            description: '',
+            avatar_url: '',
+            link: '',
+        });
+
+        // Renderizar a Tela
+    }
+
+}
